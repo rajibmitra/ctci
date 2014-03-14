5acb981b6681dd272a6e40bdca62705e713e895b
Sakis Kasampalis <faifgnu@gmail.com>
2014-03-14 22:33:17 +0100

    cleanup redundant code

--- python/Chapter 1/Question1_8/rotation.py
+++ python/Chapter 1/Question1_8/rotation.py
@@ -1,13 +1,3 @@
-# def isRotation(s1, s2):
-#     if len(s1) != len(s2):
-#         return False
-
-#     if not s1 or not s2:
-#         return False
-
-#     s2 = s2 * 2
-#     return s2.find(s1) != -1
-
 def isRotation(s1, s2):
     '''check if s1 is a rotation of s2'''
     if len(s1) != len(s2):
